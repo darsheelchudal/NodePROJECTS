dc2a63b003477493ae282838af11304ebeb9f64f
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-20 06:52:42 +0545

    taskError

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,8 +1,13 @@
 import taskModel from "../models/tasks.js";
 
 //get all tasks
-export const getAllTasks = (req, res) => {
-  res.send("all items");
+export const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await taskModel.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(501).json({ message: error });
+  }
 };
 
 //create tasks
@@ -16,8 +21,18 @@ export const createTask = async (req, res) => {
 };
 
 //get single item
-export const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+export const getTask = async (req, res) => {
+  try {
+    const { id: _id } = req.params;
+    const task = await taskModel.findOne({ _id });
+    res.json({ id: req.params.id });
+    if (!task) {
+      return res.status(404).json({ message: `No task with id :  ${_id}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 //update task
